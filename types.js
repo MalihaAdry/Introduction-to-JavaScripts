377ae0b4d12c8b0af21164d037cac8802ff5a538
MalihaAdry <maliha.adry.official@gmail.com>
2026-05-19 23:51:22 +0600

    variable-datatypes

---
+++ types.js
@@ -0,0 +1,15 @@
+//String type
+var name= "Shakib Khan";
+var address = "Andar kilaa bandarban with some Katabon";
+var country = 'Bangladesh';
+
+// boolean
+var isRich = true; //eta kintu string na. reserved word egula. jeta ase exact oitai likhte hobe.
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+
+var price = 2500;
+console.log(price);
+console.log(typeof price);
