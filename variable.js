87bb68b2da8ed956a1ddf4c32a2051ce34c09fcd
MalihaAdry <maliha.adry.official@gmail.com>
2026-05-19 12:53:08 +0600

    what is variable-how to declare?

---
+++ variable.js
@@ -0,0 +1,20 @@
+/* 
+* VARIABLE ==> vary + able
+* 5 things to declare a variable
+* Variable ar 1ta name thakbe , sathe value thakbe. chaile sei value change o kora jabe.
+*/
+var weight = 38 ;
+var price = 38 ;
+var year = 1999;
+var money = 2500;
+var age;//undefined value
+
+console.log(money);
+console.log(price);
+
+
+
+
+var age = 27;
+age = 35; //second bar value change kora hoise. r 1bar var likhle pore r var lekhar dorkar nai. just name t likhe value boshay dilei hobe
+console.log(age);
