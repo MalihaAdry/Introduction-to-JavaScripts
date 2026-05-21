9bcea2894341a36583f3550b3b0fbde86ea6923b
MalihaAdry <maliha.adry.official@gmail.com>
2026-05-22 00:23:00 +0600

    shorthands and mixed Operations explored

---
+++ mixedOP.js
@@ -0,0 +1,7 @@
+var first = 'Mobarak';
+var second = 'Tobarak';
+var fullname = first + second;
+// console.log(fullname);
+// console.log(40 + '20');
+console.log(40 - '20');
+console.log('40' - 10); 
