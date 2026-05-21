4312318e5965ddf3765384bcd781379e340abbb7
MalihaAdry <maliha.adry.official@gmail.com>
2026-05-21 23:25:55 +0600

    fundamental of js numbers

---
+++ numbers.js
@@ -0,0 +1,22 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+var applePrice = parseInt('Bangladesh');//NaN-Not a number
+
+// var mangoPrice = parseInt('20.5') ; 
+var mangoPrice = parseFloat('20.5') ; 
+
+/* ************************ */
+// console.log(orangePrice);
+// console.log(applePrice);
+// console.log(mangoPrice);
+// console.log(orangePrice + applePrice);
+// console.log(orangePrice + mangoPrice);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second ;
+// console.log(total);
+console.log(total.toFixed(5))// . er por koto ghor rakhbo + number ta string kore fele= problem
+// const num = 50 / 0;
+const num = -50 / 0;
+console.log(num);
